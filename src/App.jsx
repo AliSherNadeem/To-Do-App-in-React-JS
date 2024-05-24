6e0f3d5e0c32e2e88a0d9b3b06889d38d26e7c91
AliSherNadeem <alisher786389@gmail.com>
2024-05-24 19:37:25 +0500

    fix toggle task

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,35 @@ import { MdDeleteOutline } from "react-icons/md";
 function App() {
   const [todo, settodo] = useState("");
   const [todos, settodos] = useState([]);
-  const [showFinished, setshowFinished] = useState();
+  const [showFinished, setshowFinished] = useState(false);
 
   useEffect(() => {
     let todoString = localStorage.getItem("todos");
     if (todoString) {
-      let todos = JSON.parse(localStorage.getItem("todos"));
+      let todos = JSON.parse(todoString);
       settodos(todos);
     }
   }, []);
 
-  const saveToLS = (params) => {
+  const saveToLS = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
   };
 
-  const toggleFinished = (e) => {
+  const toggleFinished = () => {
     setshowFinished(!showFinished);
   };
 
-  const handleEdit = (e, id) => {
-    let t = todos.filter((i) => i.id === id);
-    settodo(t[0].todo);
-    // delete previous todo after getting him in input field
-    let newTodos = todos.filter((items) => {
-      return items.id !== id;
-    });
+  const handleEdit = (id) => {
+    let t = todos.find((i) => i.id === id);
+    settodo(t.todo);
+    // delete previous todo after getting it in the input field
+    let newTodos = todos.filter((items) => items.id !== id);
     settodos(newTodos);
     saveToLS();
   };
 
-  const handleDelete = (e, id) => {
-    let newTodos = todos.filter((items) => {
-      return items.id !== id;
-    });
+  const handleDelete = (id) => {
+    let newTodos = todos.filter((items) => items.id !== id);
     settodos(newTodos);
     saveToLS();
   };
@@ -57,13 +53,10 @@ function App() {
     settodo(e.target.value);
   };
 
-  const handleCheckbox = (e) => {
-    let id = e.target.name;
-    let index = todos.findIndex((items) => {
-      return items.id === id;
-    });
-    let newtodos = [...todos];
-    newtodos[index].isCompleted = !newtodos[index].isCompleted;
+  const handleCheckbox = (id) => {
+    let newtodos = todos.map((items) =>
+      items.id === id ? { ...items, isCompleted: !items.isCompleted } : items
+    );
     settodos(newtodos);
     saveToLS();
   };
@@ -71,12 +64,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-lg p-5 bg-indigo-300 text-white min-h-[80vh] w-1/2">
+      <div className="container mx-auto my-5 rounded-lg p-5 bg-indigo-400 text-white min-h-[80vh] w-1/2">
         <h1 className="font-bold text-center text-xl">
           TaskEase - Navigate Your Tasks with Ease
         </h1>
         <div className="addTodo flex flex-col gap-4">
-          <h2 className="text-lg font-bold my-4">Add a Task</h2>
+          <h2 className="text-lg font-bold my-3">Add a Task</h2>
           <input
             onChange={handleChange}
             value={todo}
@@ -86,7 +79,7 @@ function App() {
           <button
             onClick={handleAdd}
             disabled={todo.length < 3}
-            className="bg-indigo-500 hover:bg-indigo-600  disabled:bg-indigo-400 p-2 py-1 text-sm font-bold rounded-md mb-2"
+            className="bg-indigo-500 text-white hover:bg-indigo-600 disabled:bg-white disabled:text-indigo-600 p-2 py-1 text-sm font-bold rounded-md mb-2"
           >
             Add
           </button>
@@ -97,50 +90,44 @@ function App() {
           checked={showFinished}
         />{" "}
         Show Finished
-        <h2 className="text-lg font-bold mt-4">Your Tasks</h2>
-        <div className="todos">
+        <h2 className="text-lg font-bold mt-4 text-center ">Your Tasks</h2>
+        <div className="todos flex flex-col items-center">
           {todos.length === 0 && <div className="m-3">No Task to Display</div>}
-          {todos.map((items) => {
-            return (
-              (showFinished || items.isCompleted) && (
-                <div
-                  key={items.id}
-                  className="todo flex justify-between w-1/2 my-4"
-                >
-                  <div className="flex gap-4">
-                    <input
-                      name={items.id}
-                      onChange={handleCheckbox}
-                      type="checkbox"
-                      checked={items.isCompleted}
-                    />
-                    <div className={items.isCompleted ? "line-through" : ""}>
-                      {items.todo}
-                    </div>
-                  </div>
-                  <div className="buttons flex h-full">
-                    <button
-                      onClick={(e) => {
-                        handleEdit(e, items.id);
-                      }}
-                      className="bg-indigo-500 hover:bg-indigo-800 p-2 py-1 text-sm font-bold rounded-md mx-1"
-                    >
-                      <MdEditDocument />
-                    </button>
-
-                    <button
-                      onClick={(e) => {
-                        handleDelete(e, items.id);
-                      }}
-                      className="bg-indigo-500 hover:bg-indigo-800 p-2 py-1 text-sm font-bold rounded-md mx-1"
-                    >
-                      <MdDeleteOutline />
-                    </button>
+          {todos
+            .filter((items) => (showFinished ? items.isCompleted : true))
+            .map((items) => (
+              <div
+                key={items.id}
+                className="todo flex justify-between w-1/2 my-4"
+              >
+                <div className="flex gap-4">
+                  <input
+                    name={items.id}
+                    onChange={() => handleCheckbox(items.id)}
+                    type="checkbox"
+                    checked={items.isCompleted}
+                  />
+                  <div className={items.isCompleted ? "line-through" : ""}>
+                    {items.todo}
                   </div>
                 </div>
-              )
-            );
-          })}
+                <div className="buttons flex h-full ml-5">
+                  <button
+                    onClick={() => handleEdit(items.id)}
+                    className="bg-white text-indigo-600 hover:bg-indigo-600 hover:text-white p-2 py-1 text-sm font-bold rounded-md mx-1"
+                  >
+                    <MdEditDocument />
+                  </button>
+
+                  <button
+                    onClick={() => handleDelete(items.id)}
+                    className="bg-white text-indigo-600 hover:bg-indigo-600 hover:text-white p-2 py-1 text-sm font-bold rounded-md mx-1"
+                  >
+                    <MdDeleteOutline />
+                  </button>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </>
